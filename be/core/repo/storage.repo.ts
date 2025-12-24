894be4930de3b4d3e2ecf720fe019eab412348e2
Linh Tran <linh.tran@silentium.io>
2025-12-24 16:22:45 +0700

    feat(be): generate the code for storage repo

--- be/core/repo/storage.repo.ts
+++ be/core/repo/storage.repo.ts
@@ -1,10 +1,11 @@
 import prisma from "@Ciri/core/prisma";
 
-import type { Prisma } from "../../generated/prisma/client.js";
+import type { Prisma, PrismaClient } from "../../generated/prisma/client.js";
 
 export class StorageRepository {
-  async create(data: Prisma.StorageCreateInput) {
-    return prisma.storage.create({
+  async create(data: Prisma.StorageCreateInput, db?: PrismaClient | Prisma.TransactionClient) {
+    const client = db ?? prisma;
+    return client.storage.create({
       data,
     });
   }
